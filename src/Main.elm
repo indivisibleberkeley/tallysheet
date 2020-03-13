231235199b9753bee92ced8d245b6d5498bd5fe4
Sam Kohn <kohn@berkeley.edu>
2020-03-13 01:06:32 -0700

    Add reset and db synchronization to frontend

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,10 @@ module Main exposing (main)
 import Browser
 import Html exposing (button, text)
 import Html.Events exposing (onClick)
-import Json.Decode
+import Http
+import Json.Decode exposing (Decoder, field, int)
+import Debug
+import Time
 
 
 main : Program Json.Decode.Value Model Msg
@@ -27,13 +30,64 @@ init _ =
 
 type Msg
     = Increment
+    | SendReset
+    | Reset (Result Http.Error ())
+    | RetrieveCount
+    | UpdateCount (Result Http.Error Int)
+    | Pass (Result Http.Error ())
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            ( model + 1, Cmd.none )
+            ( model + 1
+            , Http.post
+                { url = "http://127.0.0.1:5000/api/increment"
+                , body = Http.emptyBody
+                , expect = Http.expectWhatever Pass
+                }
+            )
+
+        SendReset ->
+            ( model
+            , Http.post
+                { url = "http://127.0.0.1:5000/api/reset"
+                , body = Http.emptyBody
+                , expect = Http.expectWhatever Reset
+                }
+            )
+
+        Reset result ->
+            case Debug.log "Reset result" result of
+                Ok _ ->
+                    ( 0, Cmd.none )
+
+                Err _ ->
+                    ( -1, Cmd.none )
+
+        RetrieveCount ->
+            ( model
+            , Http.get
+                { url = "http://127.0.0.1:5000/api/get"
+                , expect = Http.expectJson UpdateCount tallyDecoder
+                }
+            )
+
+        UpdateCount result ->
+            case result of
+                Ok newValue ->
+                    ( newValue, Cmd.none )
+
+                Err _ ->
+                    ( -1, Cmd.none )
+
+        Pass _ ->
+            ( model, Cmd.none )
+
+tallyDecoder : Decoder Int
+tallyDecoder =
+    field "tally1" int
 
 
 view : Model -> Browser.Document Msg
@@ -42,10 +96,11 @@ view model =
     , body =
         [ text (String.fromInt model)
         , button [ onClick Increment ] [ text "+1 Call" ]
+        , button [ onClick SendReset ] [ text "reset" ]
         ]
     }
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Sub.none
+    Time.every 1000 (\_ -> RetrieveCount)
